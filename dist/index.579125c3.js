59c41da5244ba6e986094dc0fd4fa68d1cf72a7d
Michelly <Msancheza9@eafit.edu.co>
2025-03-04 16:51:43 -0500

    proyecto

---
+++ dist/index.579125c3.js
@@ -0,0 +1,20 @@
+const tareaInput = document.querySelector("#tareaInput");
+const agregarBtn = document.querySelector("#agregarBtn");
+const listaTareas = document.querySelector("#listaTareas");
+agregarBtn.addEventListener("click", ()=>{
+    const tareaTexto = tareaInput.value.trim();
+    if (!tareaTexto) {
+        alert("Por favor, ingresa una tarea");
+        return;
+    }
+    const elementoTarea = document.createElement("li");
+    elementoTarea.innerHTML = `${tareaTexto} <button class='eliminar'>X</button>`;
+    listaTareas.appendChild(elementoTarea);
+    tareaInput.value = "";
+});
+listaTareas.addEventListener("click", (evento)=>{
+    if (evento.target.classList.contains("eliminar")) evento.target.parentElement.remove();
+    else evento.target.classList.toggle("completado");
+});
+
+//# sourceMappingURL=index.579125c3.js.map
